651c21feb5e6490c904eef3a77de88b3605b8964
szybkimirek <jkrybus_221i3@e.zseeim.edu.pl>
2024-03-19 09:25:42 +0000

    studentmachine send

---
+++ python/cwiczenie2/main.py
@@ -0,0 +1,17 @@
+"""
+a=int( input("podaj temperature"))
+if a>= -5 and a< 10:
+    print ("zima")
+if a>= 0 and a< 10:
+    print ("przedwiosnie")
+if a>=0 and a< 15:
+    print ("wiosna")
+"""
+licznik = 0
+while licznik < 10:
+	z = int(input("podaj wartosc"))
+	if z == 5:
+		break
+	licznik = licznik+1
+
+
